46a7d807b7455c8111a4aaeb8887aac435c57ea3
kostarling <54403039+kostarling@users.noreply.github.com>
2024-09-18 20:37:50 +0000

    erste

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.sidebar.image("logo.webp", width=200)
+st.sidebar.title("S.T.A.R.L.I.N.G. AI")
