1116a9c120ce42c7c2e413c6a690595619dc07ab
kostarling <54403039+kostarling@users.noreply.github.com>
2024-09-24 11:17:39 +0000

    add flux

---
+++ pages/FLUX schnell.py
@@ -0,0 +1,19 @@
+
+import streamlit as st
+import requests
+HF = st.secrets["HF"]
+API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev"
+headers = {"Authorization": f"Bearer {HF}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+image_bytes = query({
+	"inputs": "Astronaut riding a tree",
+})
+# You can access the image with PIL.Image for example
+import io
+from PIL import Image
+image = Image.open(io.BytesIO(image_bytes))
+
+st.image(image)
