ddae3255b2b994da1b94082779c6cee2a1642d3a
kostarling <54403039+kostarling@users.noreply.github.com>
2024-10-01 20:45:25 +0000

    pw

--- pages/chat.py
+++ pages/chat.py
@@ -5,8 +5,12 @@ from g4f.client import Client
 from youtube_transcript_api import YouTubeTranscriptApi
 
 
-question = st.text_input("Q", value="2J4chT7jX0Q")
+from st_paywall import add_auth
 
-trans = gpt4o("get transcribe of this video: https://www.youtube.com/watch?v=2J4chT7jX0Q")
+add_auth(required=True)
 
-st.markdown(trans)
+#after authentication, the email and subscription status is stored in session state
+st.write(st.session_state.email)
+st.write(st.session_state.user_subscribed)
+
+st.text("hello")
